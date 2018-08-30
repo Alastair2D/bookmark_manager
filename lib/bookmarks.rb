ab1a0ea418ae6b5c0051b80ea8a3911b5255c63a
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-08-30 14:56:28 +0100

    add test database connection in fetch_bookmarks

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -2,21 +2,13 @@ require 'pg'
 
 class Bookmarks
 
-  def self.fetch_bookmarks
-    if in_test?
-      db_connect = PG.connect( dbname: 'bookmark_manager_test' )
-    else
-      db_connect = PG.connect( dbname: 'bookmark_manager' )
-    end
+  def self.fetch_bookmarks # this returns a tuple (a bunch of hashes)
+    ENV['RACK_ENV'] == 'test' ? '_test' : ''
+
+    db_connect = PG.connect( dbname: "bookmark_manager#{ "_test" if ENV["RACK_ENV"] == "test" }") 
     db_result = db_connect.exec( "SELECT * FROM bookmarks")
+    db_result.map { |bookmark| "#{bookmark['url']}, \n" }
   end
 
-  def self.prettify_bookmarks(input)
-    input.map { |bookmark| "#{bookmark['url']} " }
-  end
 
-  private
-  def self.in_test?
-    ENV['RACK_ENV'] == 'test' 
-  end
 end
