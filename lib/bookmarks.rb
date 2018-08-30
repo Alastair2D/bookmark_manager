5d92c981f9311011662c3654a9663c750693cf10
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-08-30 16:46:08 +0100

    fuck you Mal

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -3,11 +3,16 @@ require 'pg'
 class Bookmarks
 
   def self.fetch_bookmarks # this returns a tuple (a bunch of hashes)
-    ENV['RACK_ENV'] == 'test' ? '_test' : ''
     db_connect = PG.connect( dbname: "bookmark_manager#{ "_test" if ENV["RACK_ENV"] == "test" }") 
     db_result = db_connect.exec( "SELECT * FROM bookmarks")
     db_result.map { |bookmark| "#{bookmark['url']}, \n" }
   end
-
+  
+  def self.create(url)
+    db_connect = PG.connect( dbname: "bookmark_manager#{ "_test" if ENV["RACK_ENV"] == "test" }") 
+    # new_url_id = db_connect.exec('SELECT COUNT(*) FROM bookmarks').to_i + 1
+    new_url_id = rand(1..1000)
+    db_connect.exec("INSERT INTO bookmarks VALUES(#{new_url_id}, '#{url}')")
+  end
 
 end
--- spec/features/adding_bookmarks_features_spec.rb
+++ spec/features/adding_bookmarks_features_spec.rb
@@ -1,10 +1,16 @@
 feature 'Bookmark Manager' do
 
-  scenario 'adds a bookmark to DB bookmark_manager\'s bookmark table' do
+  scenario 'adds a bookmark to DB bookmark_manager\'s bookmark table using web helper' do
     clear_test_database
     add_bookmark_to_test_database
     visit '/bookmarks'
     expect(page).to have_content 'makers'
+  end
+  
+  scenario 'adds a bookmark to DB bookmark_manager\'s bookmark table without web helper' do
+    Bookmarks.create('testURL')
+    visit '/bookmarks'
+    expect(page).to have_content 'testURL'
   end
 
 end
