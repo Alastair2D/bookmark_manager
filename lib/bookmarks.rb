751a5d87908f375d2e18b0f4438f4b978a3cdea6
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-08-31 13:52:32 +0100

    refactor create method so that it automatically id's + 1

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -5,7 +5,7 @@ class Bookmarks
   def self.fetch_bookmarks # this returns a tuple (a bunch of hashes)
     db_connect = PG.connect( dbname: "bookmark_manager#{ "_test" if ENV["RACK_ENV"] == "test" }") 
     db_result = db_connect.exec( "SELECT * FROM bookmarks")
-    db_result.map { |bookmark| "#{bookmark['url']}, \n" }
+    db_result.map { |bookmark| "#{bookmark['url']}" }
   end
   
   def self.create(url)
