fba058ce630b9aa7bd8b744901cc2ca5f439aefc
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-02 17:11:26 +0100

    add labels

--- spec/units/bookmark_spec.rb
+++ spec/units/bookmark_spec.rb
@@ -1,28 +1,29 @@
+
 require 'bookmark'
 
 describe Bookmark do
-  # describe '.all' do
-  #   it 'returns a list of bookmarks' do
-  #     clear_test_database
-  #     # Add the test data
-  #     Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
-  #     Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
-  #     Bookmark.create(url: "http://www.google.com", title: "Google")
+  describe '.all' do
+    it 'returns a list of bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
 
-  #     bookmarks = Bookmark.all
-  #     bookmark = bookmarks.first
+      # Add the test data
+      Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
+      Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
+      Bookmark.create(url: "http://www.google.com", title: "Google")
 
-  #     expect(bookmarks.length).to eq 3
-  #     expect(bookmark).to be_a Bookmark
-  #     expect(bookmark).to respond_to(:id)
-  #     expect(bookmark.title).to eq 'Makers Academy'
-  #     expect(bookmark.url).to eq 'http://www.makersacademy.com'
-  #   end
-  # end
+      bookmarks = Bookmark.all
+      bookmark = bookmarks.first
+
+      expect(bookmarks.length).to eq 3
+      expect(bookmark).to be_a Bookmark
+      expect(bookmark).to respond_to(:id)
+      expect(bookmark.title).to eq 'Makers Academy'
+      expect(bookmark.url).to eq 'http://www.makersacademy.com'
+    end
+  end
 
   describe '.create' do
     it 'creates a new bookmark' do
-      clear_test_database
       bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
 
       expect(bookmark).to be_a Bookmark
@@ -32,15 +33,43 @@ describe Bookmark do
 
     it 'validates the URL' do
       bookmark = Bookmark.create(url: 'not a valid url', title: 'not url')
+
       expect(bookmark).not_to be_a Bookmark
     end
   end
 
   describe '.delete' do
-  it 'deletes the given bookmark' do
-    bookmark = Bookmark.create(title: 'Makers Academy', url: 'http://www.makersacademy.com')
-    Bookmark.delete(id: bookmark.id)
-    expect(Bookmark.all.length).to eq 0
+    it 'deletes the given bookmark' do
+      bookmark = Bookmark.create(title: 'Makers Academy', url: 'http://www.makersacademy.com')
+
+      Bookmark.delete(id: bookmark.id)
+
+      expect(Bookmark.all.length).to eq 0
+    end
   end
-end
+
+  describe '.update' do
+    it 'updates the bookmark with the given data' do
+      bookmark = Bookmark.create(title: 'Makers Academy', url: 'http://www.makersacademy.com')
+      updated_bookmark = Bookmark.update(id: bookmark.id, url: 'http://www.snakersacademy.com', title: 'Snakers Academy')
+
+      expect(updated_bookmark).to be_a Bookmark
+      expect(updated_bookmark.id).to eq bookmark.id
+      expect(updated_bookmark.title).to eq 'Snakers Academy'
+      expect(updated_bookmark.url).to eq 'http://www.snakersacademy.com'
+    end
+  end
+
+  describe '.find' do
+    it 'returns the requested bookmark object' do
+      bookmark = Bookmark.create(title: 'Makers Academy', url: 'http://www.makersacademy.com')
+
+      result = Bookmark.find(id: bookmark.id)
+
+      expect(result).to be_a Bookmark
+      expect(result.id).to eq bookmark.id
+      expect(result.title).to eq 'Makers Academy'
+      expect(result.url).to eq 'http://www.makersacademy.com'
+    end
+  end
 end
